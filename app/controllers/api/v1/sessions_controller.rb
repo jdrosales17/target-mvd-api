c0d4866e50861c13a81fe8e51ab4b8cd38b0fd28
Juan Diego Rosales <jdrosales17@gmail.com>
2018-07-12 14:19:21 -0300

    PR Fixes. Moving json response helper method to application controller and changing error messages tree in locales

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -10,10 +10,10 @@ module Api
           response.headers.merge!(new_auth_header)
           render_create_success
         else
-          render json: { error: I18n.t('api.sessions.facebook.not_authorized') }, status: :forbidden
+          render json: { error: I18n.t('api.sessions.facebook.forbidden') }, status: :forbidden
         end
       rescue Koala::Facebook::AuthenticationError
-        render json: { error: I18n.t('api.sessions.facebook.not_authorized') }, status: :forbidden
+        render json: { error: I18n.t('api.sessions.facebook.forbidden') }, status: :forbidden
       rescue ActiveRecord::RecordNotUnique
         render json: { error: I18n.t('api.sessions.facebook.already_registerd') }, status: :bad_request
       end
